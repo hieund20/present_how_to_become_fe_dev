b409c69d5b9ee6cbd1d8eeccffd04056b142e16e
hieudnguyen <hieudnguyen@meu-solutions.com>
2022-11-07 22:45:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Question } from "./data/question";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [questionId, setQuestionId] = useState(1);
+
+  const onNextSlide = () => {
+    if (questionId === data.length) return;
+    setQuestionId(questionId + 1);
+  };
+
+  const onPrevSlide = () => {
+    if (questionId === 1) return;
+    setQuestionId(questionId - 1);
+  };
+
+  useEffect(() => {
+    setData(Question);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          style={{ fontSize: 18, marginTop: 16 }}
         >
-          Learn React
-        </a>
+          HOW TO BECOME A FRONT-END DEVELOPER
+        </Typography>
       </header>
+      <body>
+        <Container maxWidth="xl" style={{ paddingTop: 90, paddingBottom: 100 }}>
+          {data.map((item, index) => (
+            <>
+              {questionId === item.id && (
+                <>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    style={{ fontSize: 20, marginBottom: 32, color: "white" }}
+                  >
+                    {item.question}
+                  </Typography>
+                  <Grid
+                    sx={{ flexGrow: 1, alignItems: "center" }}
+                    container
+                    spacing={2}
+                  >
+                    <Grid item xs={12}>
+                      <Grid container justifyContent="center" spacing={2}>
+                        <IconButton
+                          onClick={() => onPrevSlide()}
+                          style={{ color: "white" }}
+                        >
+                          <ArrowBackIosIcon />
+                        </IconButton>
+                        {item.option.map((el, index) => (
+                          <Grid key={index} item style={{ width: "25%" }}>
+                            <Card
+                              sx={{
+                                maxWidth: 400,
+                                minHeight: 300,
+                                height: 300,
+                              }}
+                            >
+                              <CardActionArea>
+                                <CardMedia
+                                  component="img"
+                                  height="220"
+                                  image={el.image}
+                                  alt="green iguana"
+                                />
+                                <CardContent>
+                                  <Typography
+                                    variant="body2"
+                                    color="black"
+                                    style={{ fontSize: 14, fontWeight: "bold" }}
+                                  >
+                                    {el.content}
+                                  </Typography>
+                                </CardContent>
+                              </CardActionArea>
+                            </Card>
+                          </Grid>
+                        ))}
+                        <IconButton
+                          onClick={() => onNextSlide()}
+                          style={{ marginLeft: 16, color: "white" }}
+                        >
+                          <ArrowForwardIosIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </>
+              )}
+            </>
+          ))}
+        </Container>
+      </body>
     </div>
   );
 }
---
+++ src/data/question.js
@@ -0,0 +1,65 @@
+export const Question = [
+  {
+    id: 1,
+    question: "What is the Front-End Developer ?",
+    option: [
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667829939/meu-present-how-to-become-fe-dev/ezgif.com-gif-maker_qsnxms.jpg",
+        content: "The person who focuses on developing on Client-side",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667830291/meu-present-how-to-become-fe-dev/ezgif.com-gif-maker_1_mq2wif.jpg",
+        content: "Focussing on development and interface building",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667830491/meu-present-how-to-become-fe-dev/thiet-ke-ui-va-ux-1_snmbqv.png",
+        content: "Creator of the user experience (UX) platform",
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Skills required",
+    option: [
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667830972/meu-present-how-to-become-fe-dev/t%E1%BA%A3i_xu%E1%BB%91ng_sy9cs4.png",
+        content: "Knowledge Base: HTML, CSS, JavaScript",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667831118/meu-present-how-to-become-fe-dev/1_lC1kj3IeXoE8Z3OCKJoWkw_dxf6ys.jpg",
+        content: "Framework/Libary: ReactJS, Angular, VueJS (popular)",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667831415/meu-present-how-to-become-fe-dev/ui-16073984503078_gvmjl0.jpg",
+        content: "A little UX/UI skill",
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "How to learn ?",
+    option: [
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667833523/meu-present-how-to-become-fe-dev/Untitled_design_2_loycns.png",
+        content: "Youtube, Udemy, Pluralsight, Coursera,...",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667833683/meu-present-how-to-become-fe-dev/t%E1%BA%A3i_xu%E1%BB%91ng_5_gzkehm.png",
+        content: "Practice by building pet project",
+      },
+      {
+        image:
+          "https://res.cloudinary.com/dwgjmgf6o/image/upload/v1667832966/meu-present-how-to-become-fe-dev/t%E1%BA%A3i_xu%E1%BB%91ng_1_deix1g.png",
+        content: "Don't give up :v",
+      },
+    ],
+  },
+];
